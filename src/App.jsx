bbe21cefabf487007efdab8787ea7371182c8565
Stone Osborne <stoneosborne44@gmail.com>
2024-01-30 12:38:35 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h1 className="text-xl font-bold text-red-500">Hello Tailwind with React Vite</h1>
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Button
+        Button!
       </button>
 
 {/*
