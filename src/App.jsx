a6b42713f057bce1df84028f3a124f471a11917f
Stone Osborne <stoneosborne44@gmail.com>
2024-02-06 01:41:23 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import NavBar from "./components/NavBar.jsx"
 import SmoothiePage from "./components/SmoothiePage.jsx"
+import FooterPage from "./components/FooterPage.jsx"
+import TablePage from "./components/TablePage.jsx"
 
 function App() {
   return (
     <>
       <NavBar />
+      {/* <TablePage /> */}
       <SmoothiePage />
+
+      <FooterPage />
     </>
   )
 }
